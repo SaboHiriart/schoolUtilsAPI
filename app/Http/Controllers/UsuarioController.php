2264ac4bccdbffe572f713959ce788bac2bd036c
SaboHiriart <saboochoahiriart@outlook.com>
2022-04-29 13:54:05 -0500

    First methods of the API

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+
+class UsuarioController extends Controller{
+    public function index(){
+        $datosUsuario = Usuario::all();
+        return response()->json($datosUsuario);
+    }
+
+    public function registrar(Request $request){
+        $datosUsuario = new usuario;
+        if($request->hasFile('image_route')){
+            $nombreArchivoOriginal = $request -> file('image_route') -> getClientOriginalName();
+            $nuevoNombre = "Usuario.jpg";
+            $carpetaDestino = './upload/userImage/';
+            $request -> file('image_route')->move($carpetaDestino, $nuevoNombre);
+            $datosUsuario->name = $request->name;
+            $datosUsuario->email = $request->email;
+            $datosUsuario->password = $request->password;
+            $datosUsuario->image_route = ltrim($carpetaDestino, '.').$nuevoNombre;
+    
+            $datosUsuario->save();
+        }
+        return response()->json($request);
+    }
+
+    public function consultar($id){
+        $dataUsuarios = new Usuario;
+        $datosEncontrados  = $dataUsuarios->find($id);
+        return response()->json($datosEncontrados);
+    }
+
+    public function eliminar($id){
+        $datosUsuario = Usuario::find($id);
+
+        if($datosUsuario){
+            $rutaArchivo = base_path('public').$datosUsuario->image_route;
+            if(file_exists($rutaArchivo)){
+                unlink($rutaArchivo);
+            }
+            $datosUsuario->delete();
+        }
+
+        return response()->json("Registro Borrado");
+    }
+}
---
+++ database/migrations/2022_04_29_171401_usuarios.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 250);
+            $table->string('email', 300);
+            $table->string('password', 2500);
+            $table->string('image_route', 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
